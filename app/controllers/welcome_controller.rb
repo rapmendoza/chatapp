32d37383cc6ed876ec71f26bb580f23d4b3ba392
Serafin Mendoza <mendozarap@gmail.com>
2015-06-19 12:31:45 +0800

    Cleaned up comments.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,15 +3,8 @@ class WelcomeController < ApplicationController
     @session = User.new
     @message = Message.new
     @messages = Message.last(10)
-    # @users = User.where("last_online_at > ?", DateTime.now.in_time_zone-5)
     if current_user
       @users = User.where(["id != ? AND last_online_at > ?", current_user.id, DateTime.now-5.minutes])
     end
   end
-
-  # def next_messages
-  #   @messages = Messages.last(15)
-  # end
 end
-
-#DateTime.now.in_time_zone - User.last.last_online_at
